5d5dc5bdf78c15d18432d3abf70ce27b44eb59ce
Gustavo <gustascrows@gmail.com>
2023-08-24 09:39:48 -0300

    fix(1): correcao

--- src/components/AddButton/index.tsx
+++ src/components/AddButton/index.tsx
@@ -2,10 +2,16 @@ import { AntDesign } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
 import { defaultTheme } from '../../theme/default';
 import { addButton } from './style';
+import { useNavigation } from '@react-navigation/native';
 
 const ButtonTeste = () => {
+    const navigation = useNavigation();
+
+    const navigateToProfile = () => {
+      navigation.navigate('Form');
+    };
     return (
-        <TouchableOpacity style={addButton.container}>
+        <TouchableOpacity style={addButton.container} onPress={navigateToProfile}>
             <AntDesign style={addButton.icon} name='plus' size={24} color={defaultTheme.colors.greyStrong} />
         </TouchableOpacity>
     );
