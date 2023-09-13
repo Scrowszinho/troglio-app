3870c0a63f1304698c16ceb0cc5f2b0fbd392db0
Bruno Coelho <brunoca2000@hotmail.com>
2023-09-13 14:35:44 -0300

    Hoje quero desejar um boa tarde muito especial, que a sua tarde seja iluminada pelos brilhantes raios da amizade, do amor, da paz e da esperança.

---
+++ src/components/BackDrop/index.tsx
@@ -0,0 +1,90 @@
+import { TouchableOpacity, Text, View, Button } from 'react-native';
+import { defaultTheme } from '../../theme/default';
+import { backdropStyle } from './style';
+import { useEffect, useState } from 'react';
+import Backdrop from 'react-native-modal';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { Spacer } from '@react-native-material/core';
+import { Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { Picker } from '@react-native-picker/picker';
+
+const BackDropComponent = () => {
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+    };
+
+    return (
+        <View>
+            <TouchableOpacity onPress={toggleModal}>
+                <FontAwesome5 name={'filter'} size={24} color={defaultTheme.colors.white} />
+
+                <Backdrop
+                    isVisible={isModalVisible}
+                    onBackdropPress={toggleModal}
+                    onBackButtonPress={toggleModal}
+                    backdropOpacity={0.5}
+                    animationIn="slideInUp"
+                    animationOut="slideOutDown"
+                    style={backdropStyle.modal}
+                >
+                    <View style={backdropStyle.modalContainer}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <View style={{ paddingRight: 10 }}>
+                                <Feather name="filter" size={24} color="black" />
+                            </View>
+                            <Text style={{ fontSize: 15 }}>FILTROS</Text>
+                            <Spacer></Spacer>
+                            <Ionicons name="close" size={24} color="black" onPress={toggleModal} />
+                        </View>
+
+                        <View style={{ flexDirection: 'row', paddingTop: 24 }}>
+                            <Text style={{ fontSize: 15 }}>POR MÊS</Text>
+                            <Spacer></Spacer>
+                            <View>
+                                <Feather name="plus" size={24} color="black" />
+                            </View>
+                        </View>
+
+                        {/*  
+                        <View style={{ flexDirection: 'row', paddingTop: 24 }}>
+                            <Text style={{ fontSize: 15 }}>POR ANO</Text>
+                            <Spacer></Spacer>
+                            <View>
+                                <Feather name="plus" size={24} color="black" />
+                            </View>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', paddingTop: 24 }}>
+                            <Text style={{ fontSize: 15 }}>OUTROS</Text>
+                            <Spacer></Spacer>
+                            <View>
+                                <Feather name="plus" size={24} color="black" />
+                            </View>
+                        </View> */}
+
+                    </View>
+                </Backdrop>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const _months = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+];
+
+export default BackDropComponent;
---
+++ src/components/BackDrop/style.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import { defaultTheme } from '../../theme/default';
+const screenHeight = Dimensions.get('window').height;
+
+export const backdropStyle = StyleSheet.create({
+    modal: {
+        justifyContent: 'flex-end',
+        margin: 0,
+    },
+    modalContainer: {
+        backgroundColor: 'white',
+        padding: 24,
+        paddingLeft: 32,
+        paddingRight: 32,
+        borderTopLeftRadius: 16,
+        borderTopRightRadius: 16,
+        alignItems: 'center',
+        height: screenHeight / 2,
+    }
+});
