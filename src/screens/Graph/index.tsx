7d120ed850be711d7f1fb8b64ba5b563aad4bec7
Bruno Coelho <brunoca2000@hotmail.com>
2023-09-17 11:04:13 -0300

    feat: As árvores começaram a balançar com as risadas das aves e dos animais, que agora estavam presos em bolas de chiclete coloridas.

--- src/screens/Graph/index.tsx
+++ src/screens/Graph/index.tsx
@@ -1,26 +1,38 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
 import { PieChart } from 'react-native-chart-kit';
-import { styles } from './styles';
+// import { styles } from './styles';
 import axios from '../../utils/axios';
 import { useEffect, useState } from 'react';
 import { AxiosError } from 'axios';
 import { IDebitData, IIncomingsData } from '../../interfaces/home.interface';
+import { getColorsByType } from '../../theme/default';
 
+function getMonthName(monthNumber: number) {
+    const date = new Date();
+    date.setMonth(monthNumber - 1);
+    const dateLocale = date.toLocaleString('pt-BR', { month: 'long' });
+
+    return dateLocale.substring(0, 1).toUpperCase() + dateLocale.substring(1, dateLocale.length).toLowerCase();
+}
 
 const Graph = () => {
     const [month, setMonth] = useState<number>(8);
     const [error, setError] = useState<string>('');
     const [openSnackbar, setOpenSnackbar] = useState<boolean>(false);
     const [debits, setDebits] = useState<IDebitData>();
+    // const [total, setTotal] = useState<number>(0);
     const [incomings, setIncomings] = useState<IIncomingsData>();
     const [openFilter, setOpenFilter] = useState(false);
 
     useEffect(() => {
         axios.post('/debits/year', { month: 8, year: 2023 })
             .then((response) => {
+                console.log(response.data);
+
                 setDebits(response.data);
+                // setTotal(response.data.total);
             })
             .catch((error: AxiosError<any>) => {
                 setError(error.response?.data?.errors[0]);
@@ -34,36 +46,27 @@ const Graph = () => {
         // 		setError(error.response?.data?.errors[0]);
         // 		setOpenSnackbar(true);
         // 	});
+
     }, []);
-   
-    const data = [
-        {
-            name: debits?.debits[0].month,
-            population: 10,
-            color: 'red',
+
+
+    const getDebits = () => {
+        if (!debits) return [];
+
+        return debits.debits.map((element) => ({
+            name: getMonthName(+element.month),
+            population: element.value,
+            color: getColorsByType(element.type).color,
             legendFontColor: 'black',
             legendFontSize: 15,
-        },
-        {
-            name: 'ssss',
-            population: 20,
-            color: 'green',
-            legendFontColor: 'black',
-            legendFontSize: 15,
-        },
-        {
-            name: 'ddd',
-            population: 15,
-            color: 'blue',
-            legendFontColor: 'black',
-            legendFontSize: 15,
-        },
-    ];
+        }));
+    };
+
     return (
-        <SafeAreaView>
-            <View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.chartContainer}>
                 <PieChart
-                    data={data}
+                    data={getDebits()}
                     width={300}
                     height={200}
                     chartConfig={{
@@ -77,8 +80,64 @@ const Graph = () => {
                     absolute
                 />
             </View>
+            <View style={styles.cardContainer}>
+                <View style={styles.card}>
+                    <Text style={styles.legend}>Legenda:</Text>
+                    <Text ></Text>
+                    {getDebits().map((data, index) => (
+                        <View key={index} style={styles.legendItem}>
+                            <Text>{data.name}</Text>
+                        </View>
+                    ))}
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.totalLabel}>Total:</Text>
+                    <Text style={styles.totalValue}>{debits?.total}</Text>
+                </View>
+            </View>
         </SafeAreaView>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    chartContainer: {
+        marginTop: 20,
+    },
+    cardContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        marginTop: 20,
+    },
+    card: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 10,
+        width: '48%',
+    },
+    legend: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    legendItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 5,
+    },
+    totalLabel: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    totalValue: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
 
 export default Graph;
